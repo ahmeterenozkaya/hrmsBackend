6310520c2b69d1bc5d6ef605e78ba77c163c6afd
Ahmet Eren Özkaya <68692196+ahmeterenozkaya@users.noreply.github.com>
2022-08-05 08:14:15 -0600

    Delete hrms directory

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/UserDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.hrms.entities.abstracts.Users;
-
-public interface UserDao extends JpaRepository<Users, Integer>{
-
-}
