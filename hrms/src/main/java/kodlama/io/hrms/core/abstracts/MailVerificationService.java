9f3f09d0bcfbc45e7b74d8f55a1eb435357fbde6
Ahmet Eren Özkaya <68692196+ahmeterenozkaya@users.noreply.github.com>
2022-08-08 02:07:54 -0600

    Delete hrms directory

--- hrms/src/main/java/kodlama/io/hrms/core/abstracts/MailVerificationService.java
+++
@@ -1,7 +0,0 @@
-package kodlama.io.hrms.core.abstracts;
-
-import kodlama.io.hrms.core.utilities.results.Result;
-
-public interface MailVerificationService {
-	Result mailVerification(String email);
-}
